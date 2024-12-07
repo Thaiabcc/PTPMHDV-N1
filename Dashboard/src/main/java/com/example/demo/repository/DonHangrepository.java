5141dbebdcbd39a812c0cca3f59b42264281db49
Thaiabcc <ducthai2182004@gmail.com>
2024-12-07 10:56:19 +0700

    Delete Dashboard directory

--- Dashboard/src/main/java/com/example/demo/repository/DonHangrepository.java
+++
@@ -1,9 +0,0 @@
-package com.example.demo.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.example.demo.model.DonHang;
-
-public interface DonHangrepository extends JpaRepository<DonHang, Integer> {
-
-}
